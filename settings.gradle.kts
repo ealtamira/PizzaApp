fccaa1106abaf37ff53ba6e10df5d9e71e0e0fca
ealtamira <ealtamirano@student.neumont.edu>
2024-02-01 11:51:23 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "E2-3PizzaApp"
+include(":app")
